55bc743d8195c938acc9536a7a407f9ec5c91e06
Srinivasan444 <srini972003@gmail.com>
2024-11-16 14:38:11 +0530

    upload

--- Gmart-grocery-MERN-stack-webapp/README.md
+++ Gmart-grocery-MERN-stack-webapp/README.md
@@ -1,4 +1,4 @@
-# ecommerce-app-2023
+# G-MART-2024
 complete mern stack ecommerce project 2023 complete code
 # Please check branches source code is video no wise 
 ## here is project playlist : https://youtube.com/playlist?list=PLuHGmgpyHfRzhGkSUfY0vpi67X64g0mXB
--- Gmart-grocery-MERN-stack-webapp/client/src/pages/Auth/ForgotPasssword.js
+++ Gmart-grocery-MERN-stack-webapp/client/src/pages/Auth/ForgotPasssword.js
@@ -34,7 +34,7 @@ const ForgotPasssword = () => {
     }
   };
   return (
-    <Layout title={"Forgot Password - Ecommerce APP"}>
+    <Layout title={"Forgot Password - G-MART"}>
       <div className="form-container ">
         <form onSubmit={handleSubmit}>
           <h4 className="title">RESET PASSWORD</h4>
